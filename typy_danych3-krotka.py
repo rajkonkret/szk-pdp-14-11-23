52a84cbc22dc235cb7314632a063c6ba95c11538
CSComarch <rajkonkret660@gmail.com>
2023-11-14 14:43:47 +0100

    update typy_danych3-krotka.py

--- typy_danych3-krotka.py
+++ typy_danych3-krotka.py
@@ -15,3 +15,34 @@ print(type(tupla3))  # <class 'tuple'>
 print(tupla2.index("Tomek"))  # indeks 0 bo pierwszy element
 print(tupla2.count("Asia"))  # indeks 1
 # 14:30
+
+# rozpakowanie krotki
+a, b = 1, 2  # po prawej stronie mamy krotkę
+print(a)
+print(b)
+
+tp = 1, 2, 3
+print(type(tp))  # <class 'tuple'>
+
+# a, b = tp  # ValueError: too many values to unpack (expected 2) - za mało zmiennych
+a, *b = tp  # * - worek na pozostałe dane (moze byc tylko jedna gwiazdka *)
+print(a, b)  # 1 [2, 3]
+print(tupla2)  # ('Tomek', 'Asia', 'Zbyszek', 'Marcin')
+imie1, imie2, imie3, imie4 = tupla2
+imie1, *imie2, imie3 = tupla2
+print(imie1)  # Tomek
+print(imie2)  # ['Asia', 'Zbyszek']
+print(imie3)  # Marcin
+
+tupla4 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+a, *b, c = tupla4
+print(a)
+print(b)
+print(c)
+# 1
+# [2, 3, 4, 5, 6, 7, 8]
+# 9
+
+lista = list(tupla2)
+print(lista)  # ['Tomek', 'Asia', 'Zbyszek', 'Marcin']
+print(type(lista))  # <class 'list'>
