21274b2e91afdd2493bf85548f807fe9c940915e
CSComarch <rajkonkret660@gmail.com>
2023-11-15 09:54:51 +0100

    2 - add warunki

--- warunki.py
+++ warunki.py
@@ -11,17 +11,67 @@ else:  # w przeciwnym przypadku (działanie domyślne)
     print("Musisz się uczyc dalej")
 
 print("Program idzie dalej")
-
-podatek = 0
-zarobki = int(input("Podaj dochód"))  # zamieniam na int()
-if zarobki < 10000:
-    podatek = 0.0
-elif zarobki < 30000:
-    podatek = 0.2
-elif zarobki < 100000:
-    podatek = 0.4
-else:
-    podatek = 0.6
-
-print(f"Zapłacisz {zarobki * podatek} zł")
+#
+# podatek = 0
+# zarobki = int(input("Podaj dochód"))  # zamieniam na int()
+# if zarobki < 10000:
+#     podatek = 0.0
+# elif zarobki < 30000:  # 10000 .. 29999
+#     podatek = 0.2
+# elif zarobki < 100000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.6
+#
+# print(f"Zapłacisz {zarobki * podatek} zł")
 # dodac dla przedziału 10000 - 29999 podatek 0.2
+
+sum_zam = 50
+if sum_zam > 100:
+    rabacik = 25
+else:
+    rabacik = 0
+print(f"Rabat wynosi {rabacik}")
+rabat = 25 if sum_zam > 100 else 0  # to co po lewj gdy prawda, to co po prawej gdy fałsz
+# gdy warunek spełniony (True) do zmiennej rabat trafi 25,
+# gdy warunek niespełniony do zmiennej rabat trafi 0
+print(f"Rabat wynosi {rabat}")
+
+# zasymulejemy system, który będzie dostawał informacje z róznych źródeł (console, email)
+# w zależnosci z jakiego systemu i jaki bład przyjdzie podejmiemy rózne działanie
+alert_system = 'email'
+error = 'medium'
+
+error_message = "Stało się coś strasznego"
+
+lista_bledow = []
+if alert_system == 'console':
+    print(error_message)
+elif alert_system == "email":
+    print("Alert z sytemu email")
+    if error == 'medium':
+        lista_bledow.append("ostrzezenie")
+    # dla błedu critical -> krytyczny, dla pozostałych -> inny
+    elif error == 'critical':
+        lista_bledow.append('krytyczny')
+    else:
+        lista_bledow.append('inny')
+else:
+    print("Nie znam takiego systemu")
+print(lista_bledow)
+# Alert z sytemu email
+# ['ostrzezenie']
+
+# napisac test z....
+# wyswietlic pytanie -> print
+# pobrac odpowiedz -> input (mozna tez wyswietlic tu pytanie)
+# sprawdzic odpowiedz -> if else
+# wyswietlic wynik -> print
+
+odp = input("Podaj datę Chrztu Polski")  # str
+if odp == '966':
+    print("Brawo")
+else:
+    print("Masz w książce na stronie 23")
+
+# if elif else
